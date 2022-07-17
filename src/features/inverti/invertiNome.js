cc6222b4af5a7cf4e675acfdeb251eda7cb3a851
Gerardo Celozzi <93271693+GerardoCelozzi@users.noreply.github.com>
2022-07-17 11:12:14 +0200

    Update invertiNome.js

--- src/features/inverti/invertiNome.js
+++ src/features/inverti/invertiNome.js
@@ -29,15 +29,6 @@ export const nomi = createSlice({
 
 export const { change, marco, incrementByAmount } = nomi.actions
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount) => (dispatch) => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount))
-  }, 1000)
-}
 
 /*
  La funzione seguente selectCount è chiamata selettore e ci consente di selezionare un valore dallo stato. 
